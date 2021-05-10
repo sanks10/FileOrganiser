ed0892c8a34d86c16ac69d1675fcd453aca06315
Sankalp <sankalpbanodia23@gmail.com>
2021-05-10 21:50:43 +0530

    manipulating file data using fs module

--- filesystem.js
+++ filesystem.js
@@ -1,3 +1,8 @@
 let fs=require('fs');
 let data=fs.readFileSync('abc.txt');
+//readfilesync reads the content of the file whose path has been given
 console.log(data.toString());
+//we used toString function to convert buffer into string
+let wdata ='demo text I wanna add to a file thats gonna be created using a jsmodule';
+//we'll use writefilesync which is an object of fs module
+fs.writeFileSync('write.txt',wdata);
