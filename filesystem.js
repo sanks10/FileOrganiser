cb9b37a129fbcd1eb4cb8efc1381b43b132914d6
Sankalp <sankalpbanodia23@gmail.com>
2021-05-10 22:08:38 +0530

    created a folder

--- filesystem.js
+++ filesystem.js
@@ -1,7 +1,7 @@
 let fs=require('fs');
-let data=fs.readFileSync('abc.txt');
+//let data=fs.readFileSync('abc.txt');
 //readfilesync reads the content of the file whose path has been given
-console.log(data.toString());
+//console.log(data.toString());
 //we used toString function to convert buffer into string
 let wdata ='demo text I wanna add to a file thats gonna be created using a jsmodule';
 //we'll use writefilesync which is an object of fs module
@@ -10,4 +10,7 @@ fs.writeFileSync('write.txt',wdata);
 //hence, we'll use appendfile sync to add data 
 fs.appendFileSync('write.txt','\nthis is the data I wanted to append');
 //unlinkSync will be used to delete a file
-fs.unlinkSync('abc.txt');
+//fs.unlinkSync('abc.txt');
+//creating a folder using js
+fs.mkdirSync('folder');
+
